81af464a4d4e8821e2486fe66c5dce4aa2552f27
Syed Ali Safi <Syedali.safi@conovoinc.com>
2025-09-08 18:36:49 +0500

    eb configs etc

---
+++ .platform/hooks/postdeploy/x_make_folders_writable.sh
@@ -0,0 +1,6 @@
+sudo chmod -R 777 storage/
+sudo chmod -R 777 bootstrap/cache/
+
+# Storage Symlink Creation
+
+php artisan storage:lin
---
+++ .platform/hooks/predeploy/01_install_php_deps.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+yum install -y \
+  php-bcmath \
+  php-calendar \
+  php-ctype \
+  php-curl \
+  php-dom \
+  php-exif \
+  php-ffi \
+  php-fileinfo \
+  php-filter \
+  php-ftp \
+  php-gettext \
+  php-hash \
+  php-iconv \
+  php-intl \
+  php-json \
+  php-libxml \
+  php-mbstring \
+  php-mysqli \
+  php-mysqlnd \
+  php-openssl \
+  php-pcntl \
+  php-pcre \
+  php-pdo \
+  php-pdo_mysql \
+  php-pdo_sqlite \
+  php-phar \
+  php-posix \
+  php-random \
+  php-readline \
+  php-reflection \
+  php-session \
+  php-shmop \
+  php-simplexml \
+  php-sockets \
+  php-sodium \
+  php-spl \
+  php-sqlite3 \
+  php-standard \
+  php-sysvmsg \
+  php-sysvsem \
+  php-sysvshm \
+  php-tokenizer \
+  php-xml \
+  php-xmlreader \
+  php-xmlwriter \
+  php-xsl \
+  php-opcache \
+  php-zip \
+  php-zlib
+
+
